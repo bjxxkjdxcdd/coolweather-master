2221aaeecebe59b33bb80891d278d2a417004e2e
bjxxkjdxcdd <1729114498@qq.com>
2018-05-30 19:44:44 +0800

    Initial commit

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,26 @@
+package com.coolweather.android;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //先从SharedPreferences文件中读取缓存数据，
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //如果不为空就说明以前已经请求过天气数据，就没必要让用户再次选择城市，而是直接跳转到WeatherActivity
+        if (prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
